c653b9237d119f1574abd86bfb7b8fe2a0d0f36a
JonathanParra467 <jparra1786@students.mchenry.edu>
2025-11-15 21:07:16 -0600

    Add files via upload

--- object-oriented.py
+++ object-oriented.py
@@ -1,3 +1,24 @@
+"""
+Create the Pet Class:
+Define a Pet class with attributes: kind (e.g., dog, cat), breed, and name.
+Implement get and set methods for each of these attributes.
+Add a method called print_details that prints the details of the pet.
+Create Instances:
+Create three objects of the Pet class with different kinds, breeds, and names.
+
+Call the print_details method for each object that you created
+
+Demonstrate a Special Method or Function:
+Choose three of the following and demonstrate its use with the Pet class instances:
+
+__name__: Display the name of the class.
+type(): Show the class used to instantiate a pet object.
+__module__: Return the module name in which the Pet class is defined.
+__bases__: Display the base classes of the Pet class (if any).
+isinstance(): Check if an instance is of the Pet class.
+"""
+
+
 class Pet:
 
     pet_name = "kendrick"
@@ -18,10 +39,10 @@ class Pet:
     def get_name(self):
         return self.__name
     
-    def set_name(self, value):
+    def set_kind(self, value):
         self.__kind = value
         
-    def set_name(self, value):
+    def set_breed(self, value):
         self.__breed = value
 
     def set_name(self, value):
@@ -43,18 +64,21 @@ class Pet:
 
 def main():
 
-        pet1 = Pet("bird", "falcon", "brandon")
-        pet2 = Pet("kat", "ket", "fred")
-        pet3 = Pet("dog", "poodle", "john")
-
+        pet1 = Pet("Bird", "Falcon", "Brandon")
+        pet2 = Pet("Cat", "Ragdoll", "Fred")
+        pet3 = Pet("Dog", "Poodle", "John")
+        
+        print("pet details: ")
         print(pet1.get_kind())
         print(pet1.get_breed())
         print(pet1.get_name())
 
+        print("pet details: ")
         print(pet2.get_kind())
         print(pet2.get_breed())
         print(pet2.get_name())
 
+        print("pet details: ")
         print(pet3.get_kind())
         print(pet3.get_breed())
         print(pet3.get_name())
